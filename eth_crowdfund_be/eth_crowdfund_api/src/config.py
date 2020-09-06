f145efa4bb3aa5cb35514933d4116ba546143b32
Antonio Jackson <antoniojackson@gmail.com>
2020-09-06 16:39:53 -0600

    Adding dotenv environment variable functionality

--- eth_crowdfund_be/eth_crowdfund_api/src/config.py
+++ eth_crowdfund_be/eth_crowdfund_api/src/config.py
@@ -5,13 +5,13 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/eth_crowdfund_api_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 
 class Production(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/eth_crowdfund_api_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 class Testing(object):
