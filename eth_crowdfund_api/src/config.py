4648cac9f1309a8ae773d5b295a00e6222a985ee
Ross Perry <perryr16@gmail.com>
2020-09-07 10:11:30 -0600

    move .git file

--- eth_crowdfund_api/src/config.py
+++ eth_crowdfund_api/src/config.py
@@ -4,7 +4,7 @@ import os
 class Development(object):
     DEBUG = True
     TESTING = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 
@@ -12,11 +12,11 @@ class Production(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 class Testing(object):
   TESTING = True
-  JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+  # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
   SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_TEST_DATABASE_URI')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
 
