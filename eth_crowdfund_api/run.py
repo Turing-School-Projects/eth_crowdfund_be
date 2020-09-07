26c8b91e291e14a3faa8a8c61f20a1c2071367e9
Ross Perry <perryr16@gmail.com>
2020-09-07 10:31:36 -0600

    modify run file

--- eth_crowdfund_api/run.py
+++ eth_crowdfund_api/run.py
@@ -2,11 +2,13 @@ import os
 from src.seeds.seed import add_seeds
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
+  port = os.getenv('PORT')
   # run app
-  app.run(port=3000)
+  app.run(host='0.0.0.0', port=port)
 
   with app.app_context():
     add_seeds()
