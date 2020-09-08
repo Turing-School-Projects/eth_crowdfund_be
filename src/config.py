e38519e86cc40f32031e1ee48f3ab6f3b82d4292
Ross Perry <perryr16@gmail.com>
2020-09-08 11:42:58 -0600

    change prod db env

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ class Development(object):
 class Production(object):
   DEBUG = True
   TESTING = False
-  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+  SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class Testing(object):
