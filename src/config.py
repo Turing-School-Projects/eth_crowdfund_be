95e34525101a068931e3bebf23d771034c8d50f4
Ross Perry <perryr16@gmail.com>
2020-09-07 19:19:20 -0600

    modify config.py vars

--- src/config.py
+++ src/config.py
@@ -1,25 +1,19 @@
 import os
-import psycopg2
 
 
 class Development(object):
   DEBUG = True
   TESTING = False
-  # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
   SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 
 class Production(object):
   DEBUG = True
   TESTING = False
-  # SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-  SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
-  # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-  # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+  SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 class Testing(object):
   TESTING = True
-  # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
   SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_TEST_DATABASE_URI')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
 
