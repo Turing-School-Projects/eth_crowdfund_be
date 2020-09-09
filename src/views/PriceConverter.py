540ae424bdf9351561596f266415654b1e5dcda1
Taylor Keller <takeller17@gmail.com>
2020-09-08 22:42:20 -0600

    Refactors price converter with a service object

--- src/views/PriceConverter.py
+++ src/views/PriceConverter.py
@@ -1,12 +1,12 @@
 from flask import request, json, Response, Blueprint, jsonify 
+from ..services.ConversionService import ConversionService
 import requests
 
 price_converter_api = Blueprint('price_converter_api', __name__)
 
 @price_converter_api.route('/', methods=['GET'])
 def eth_to_usd(): 
-    eth_amount = float(request.args['eth'])
-    response = requests.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD')
-    conversion_rate = response.json()['USD'] 
-    value_in_usd = eth_amount * conversion_rate
-    return jsonify(value=value_in_usd)
+    wei = float(request.args['wei'])
+    conversion_service = ConversionService()
+    value_in_usd = conversion_service.wei_to_usd(wei)
+    return jsonify(USD=value_in_usd)
