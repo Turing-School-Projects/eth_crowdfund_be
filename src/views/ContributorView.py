d305f9ddca28284e35cca8c6645e2931a7559874
Antonio Jackson <antoniojackson@gmail.com>
2020-09-10 16:36:33 -0600

    Updating ContributorView ot import the CampaignSchema and add the get_campaigns route

--- src/views/ContributorView.py
+++ src/views/ContributorView.py
@@ -2,11 +2,13 @@ from flask import request, json, Response, Blueprint, g, jsonify
 from marshmallow import ValidationError
 from sqlalchemy import exc
 from ..models.Contributor import Contributor, ContributorSchema
+from ..models.Campaign import Campaign, CampaignSchema
 from . import custom_response
 
 
 contributor_api = Blueprint('contributor_api', __name__)
 contributor_schema = ContributorSchema()
+campaign_schema = CampaignSchema()
 
 @contributor_api.route('/', methods=['POST'])
 def create():
@@ -23,27 +25,6 @@ def create():
   contributor_data = contributor_schema.dump(contributor_info)
   return custom_response(contributor_data, 201)
 
-# @request_api.route('/', methods=['GET'])
-# def get_all_requests():
-#   requests = Request.get_all_requests()
-#
-#   if requests is None:
-#     return custom_response({'error': 'No Requests found'}, 404)
-#
-#   request_data = []
-#   for campaign_request in requests:
-#     request_data.append(request_schema.dump(campaign_request))
-#   return custom_response(request_data, 200)
-#
-# @request_api.route('/<int:request_id>', methods=['GET'])
-# def get_a_request(request_id):
-#   campaign_request = Request.get_one_request(request_id)
-#   if campaign_request is None:
-#     return custom_response({ 'error': 'Request not found'}, 404)
-#
-#   campaign_request_data = request_schema.dump(campaign_request)
-#   return custom_response(campaign_request_data, 200)
-#
 @contributor_api.route('/<int:contributor_id>', methods=['PUT'])
 def update(contributor_id):
 
@@ -61,14 +42,19 @@ def update(contributor_id):
 
   output_data = contributor_schema.dump(contributor_request)
   return custom_response(output_data, 200)
-#
-# @request_api.route('/<int:request_id>', methods=['DELETE'])
-# def delete(request_id):
-#   campaign_request = Request.get_one_request(request_id)
-#
-#   if campaign_request is None:
-#     return custom_response({ 'error': 'Request not found'}, 404)
-#
-#   campaign_request.delete()
-#   campaign_request_data = request_schema.dump(campaign_request)
-#   return custom_response(campaign_request_data, 200)
+
+@contributor_api.route('/<contributor_address>/campaigns', methods=['GET'])
+def get_campaigns(contributor_address):
+  try:
+    contributor = Contributor.get_contributor_by_address(contributor_address).first()
+  except ValidationError as err:
+    return custom_response(err.messages, 400)
+  except exc.IntegrityError as err:
+    return custom_response({ "error": err.orig.diag.message_detail }, 400)
+  except:
+    return custom_response({ "error": "No input data provided" }, 400)
+  campaigns = contributor.contributions
+  output_data = []
+  for campaign in campaigns:
+      output_data.append(campaign_schema.dump(campaign))
+  return custom_response(output_data, 200)
